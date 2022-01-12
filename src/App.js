02e603b65baa2b973ba0638d307dc1eef2456fa0
khanhnhqdev <khanh.nhq@tripi.vn>
2022-01-12 21:52:38 +0700

    pass test

--- src/App.js
+++ src/App.js
@@ -2,16 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello world, hot reload
-        </p>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo"/>
+                <p>
+                    learn react
+                </p>
+            </header>
+        </div>
+    );
 }
 
 export default App;
